222971f574e9b285426c901a8795be6925b6d613
Fritz Tri Yofanka Anggito Marpaung <if321035@students.del.ac.id>
2023-07-27 12:53:32 +0700

    Push Proyek

---
+++ Javascript/dialogWisata.js
@@ -0,0 +1,67 @@
+$(function() {
+    $("#btnShowPopup0").click(function() {
+        var title = "Greetings";
+        var body = "Welcome to ASPSnippets.com";
+
+        $("#MyPopup0 .modal-title").html(title);
+        $("#MyPopup0 .modal-body").html(body);
+        $("#MyPopup0").modal("show");
+    });
+});
+
+$(function() {
+    $("#btnShowPopup1").click(function() {
+        var title = "Greetings";
+        var body = "Welcome to ASPSnippets.com";
+
+        $("#MyPopup1 .modal-title").html(title);
+        $("#MyPopup1 .modal-body").html(body);
+        $("#MyPopup1").modal("show");
+    });
+});
+
+
+$(function() {
+    $("#btnShowPopup2").click(function() {
+        var title = "Greetings";
+        var body = "Welcome to ASPSnippets.com";
+
+        $("#MyPopup2 .modal-title").html(title);
+        $("#MyPopup2 .modal-body").html(body);
+        $("#MyPopup2").modal("show");
+    });
+});
+
+$(function() {
+    $("#btnShowPopup3").click(function() {
+        var title = "Greetings";
+        var body = "Welcome to ASPSnippets.com";
+
+        $("#MyPopup3 .modal-title").html(title);
+        $("#MyPopup3 .modal-body").html(body);
+        $("#MyPopup3").modal("show");
+    });
+});
+
+$(function() {
+    $("#btnShowPopup4").click(function() {
+        var title = "Greetings";
+        var body = "Welcome to ASPSnippets.com";
+
+        $("#MyPopup4 .modal-title").html(title);
+        $("#MyPopup4 .modal-body").html(body);
+        $("#MyPopup4").modal("show");
+    });
+});
+
+$(function() {
+    $("#btnShowPopup5").click(function() {
+        var title = "Greetings";
+        var body = "Welcome to ASPSnippets.com";
+
+        $("#MyPopup5 .modal-title").html(title);
+        $("#MyPopup5 .modal-body").html(body);
+        $("#MyPopup5").modal("show");
+    });
+});
+
---
+++ Javascript/dist/dialogWisata.dev.js
@@ -0,0 +1,92 @@
+"use strict";
+
+$(function () {
+  $("#btnShowPopup0").click(function () {
+    var title = "Greetings";
+    var body = "Welcome to ASPSnippets.com";
+    $("#MyPopup0 .modal-title").html(title);
+    $("#MyPopup0 .modal-body").html(body);
+    $("#MyPopup0").modal("show");
+  });
+});
+$(function () {
+  $("#btnShowPopup2").click(function () {
+    var title = "Greetings";
+    var body = "Welcome to ASPSnippets.com";
+    $("#MyPopup2 .modal-title").html(title);
+    $("#MyPopup2 .modal-body").html(body);
+    $("#MyPopup2").modal("show");
+  });
+});
+$(function () {
+  $("#btnShowPopup3").click(function () {
+    var title = "Greetings";
+    var body = "Welcome to ASPSnippets.com";
+    $("#MyPopup3 .modal-title").html(title);
+    $("#MyPopup3 .modal-body").html(body);
+    $("#MyPopup3").modal("show");
+  });
+});
+$(function () {
+  $("#btnShowPopup4").click(function () {
+    var title = "Greetings";
+    var body = "Welcome to ASPSnippets.com";
+    $("#MyPopup4 .modal-title").html(title);
+    $("#MyPopup4 .modal-body").html(body);
+    $("#MyPopup4").modal("show");
+  });
+});
+$(function () {
+  $("#btnShowPopup5").click(function () {
+    var title = "Greetings";
+    var body = "Welcome to ASPSnippets.com";
+    $("#MyPopup5 .modal-title").html(title);
+    $("#MyPopup5 .modal-body").html(body);
+    $("#MyPopup5").modal("show");
+  });
+});
+$(function () {
+  $("#btnShowPopup6").click(function () {
+    var title = "Greetings";
+    var body = "Welcome to ASPSnippets.com";
+    $("#MyPopup6 .modal-title").html(title);
+    $("#MyPopup6 .modal-body").html(body);
+    $("#MyPopup6").modal("show");
+  });
+});
+$(function () {
+  $("#btnShowPopup7").click(function () {
+    var title = "Greetings";
+    var body = "Welcome to ASPSnippets.com";
+    $("#MyPopup7 .modal-title").html(title);
+    $("#MyPopup7 .modal-body").html(body);
+    $("#MyPopup7").modal("show");
+  });
+});
+$(function () {
+  $("#btnShowPopup8").click(function () {
+    var title = "Greetings";
+    var body = "Welcome to ASPSnippets.com";
+    $("#MyPopup8 .modal-title").html(title);
+    $("#MyPopup8 .modal-body").html(body);
+    $("#MyPopup8").modal("show");
+  });
+});
+$(function () {
+  $("#btnShowPopup9").click(function () {
+    var title = "Greetings";
+    var body = "Welcome to ASPSnippets.com";
+    $("#MyPopup9 .modal-title").html(title);
+    $("#MyPopup9 .modal-body").html(body);
+    $("#MyPopup9").modal("show");
+  });
+});
+$(function () {
+  $("#btnShowPopup10").click(function () {
+    var title = "Greetings";
+    var body = "Welcome to ASPSnippets.com";
+    $("#MyPopup10 .modal-title").html(title);
+    $("#MyPopup10 .modal-body").html(body);
+    $("#MyPopup10").modal("show");
+  });
+});
